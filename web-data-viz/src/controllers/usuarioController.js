58c7614cd4b716d4161acba77d3dddfbeeec17b0
AncelmoSIlva10 <ancelmo.ssousa@sptech.school>
2026-04-24 00:15:08 -0300

    Adicionando a tela de cadastro, login e inicialização da dashboard, com integração da API

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -19,21 +19,14 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.json({
+                            idUsuario: resultadoAutenticar[0].idUsuario,
+                            nickname: resultadoAutenticar[0].nickname,
+                            id_player: resultadoAutenticar[0].id_player,
+                            qt_pontos: resultadoAutenticar[0].senha,
+                            email: resultadoAutenticar[0].email,
+                        });
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
