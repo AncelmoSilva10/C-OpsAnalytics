76cd4bfee43f82f7693355b5330e6d01c0d018a2
AncelmoSIlva10 <ancelmo.ssousa@sptech.school>
2026-04-23 23:01:48 -0300

    Adicionando a API, e fazendo cadastro de usuario no banco de dados

---
+++ web-data-viz/src/models/usuarioModel.js
@@ -0,0 +1,29 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nick, idPlayer, qtPonto, email, senha) {
+   
+    console.log("ACESSEI O USUARIO MODEL \n function cadastrar():", nick, idPlayer, qtPonto, email, senha);
+
+    var instrucaoSql = `
+        INSERT INTO usuario(nickname, id_player, qt_pontos, email, senha) 
+        VALUES ('${nick}', ${idPlayer}, ${qtPonto}, '${email}', '${senha}');
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
