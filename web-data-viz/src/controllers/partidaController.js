94d64a7c8bd94170286a2b6e8ef4b938d2f3e220
AncelmoSIlva10 <ancelmo.ssousa@sptech.school>
2026-04-27 17:30:13 -0300

    Adicionando mais um campo no banco de dados, e atualizando toda a estrutura necessária(Partida)

--- web-data-viz/src/controllers/partidaController.js
+++ web-data-viz/src/controllers/partidaController.js
@@ -5,6 +5,7 @@ function dadosPartida(req, res) {
     var duracao = req.body.duracaoServer;
     var roundsTR = req.body.roundsTRServer;
     var roundsCT = req.body.roundsCTServer;
+    var qtPontos = req.body.qtPontosServer;
     var usuario = req.body.usuarioServer;
     var resultado = req.body.resultadoServer;
     var mapa = req.body.mapaServer;
@@ -16,13 +17,15 @@ function dadosPartida(req, res) {
         res.status(400).send("A quantidade de rounds de TR está undefined!");
     } else if (roundsCT == undefined) {
         res.status(400).send("A quantidade de rounds de CT está undefined!");
+    } else if (qtPontos == undefined) {
+        res.status(400).send("A quantidade de pontos está undefined!");
     } else if (mapa == undefined) {
         res.status(400).send("O número do mapa está undefined!");
     } else if (usuario == undefined) {
         res.status(400).send("O ID do usuário está undefined!");
     } else {
-        
-        partidaModel.cadastrarPartida(data_partida, duracao, roundsTR, roundsCT, resultado, usuario, mapa)
+
+        partidaModel.cadastrarPartida(data_partida, duracao, roundsTR, roundsCT, qtPontos, resultado, usuario, mapa)
             .then((resultadoBanco) => {
                 res.status(200).json(resultadoBanco);
             })
--- web-data-viz/src/models/partidaModel.js
+++ web-data-viz/src/models/partidaModel.js
@@ -1,14 +1,14 @@
 var database = require("../database/config")
 
-function cadastrarPartida(data_partida, duracao, roundsTR, roundsCT, resultado, usuario, mapa) {
-    console.log("ACESSEI O PARTIDA MODEL \n function cadastrarPartida():", data_partida, duracao, roundsTR, roundsCT, resultado, usuario, mapa);
+function cadastrarPartida(data_partida, duracao, roundsTR, roundsCT, qtPontos, resultado, usuario, mapa) {
+    console.log("ACESSEI O PARTIDA MODEL \n function cadastrarPartida():", data_partida, duracao, roundsTR, roundsCT, qtPontos, resultado, usuario, mapa);
 
    
     var sqlData = (data_partida == "" || data_partida == null) ? "CURRENT_TIMESTAMP" : `'${data_partida}'`;
 
     var instrucaoSql = `
-            INSERT INTO partida(data_partida, duracao_media, rounds_TR, rounds_CT, resultado, fk_usuario, fk_mapa) 
-            VALUES (${sqlData}, ${duracao}, ${roundsTR}, ${roundsCT}, ${resultado}, ${usuario}, ${mapa});
+            INSERT INTO partida(data_partida, duracao_media, rounds_TR, rounds_CT, qt_pontos,resultado, fk_usuario, fk_mapa) 
+            VALUES (${sqlData}, ${duracao}, ${roundsTR}, ${roundsCT}, ${qtPontos}, ${resultado}, ${usuario}, ${mapa});
         `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
