1b0c7873dd03d75b5922679c0cadd7a55af6f122
AncelmoSIlva10 <ancelmo.ssousa@sptech.school>
2026-05-10 14:10:40 -0300

    feature: adicionando integração com o banco de dados metricas/graficos de historico

--- web-data-viz/src/routes/metricas.js
+++ web-data-viz/src/routes/metricas.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 var metricasController = require("../controllers/metricasController");
 
-router.get("/historicoPartida/:idUsuario", function (req, res) {
+router.get("/winRatingMapa/:idUsuario", function (req, res) {
     metricasController.buscarWinRatingPorMapa(req, res);
+});
+
+router.get("/historicoPartida/:idUsuario", function (req, res) {
+    metricasController.buscarHistorico(req, res);
 });
 
 
